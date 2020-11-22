a2ea0012856e90730b02990c4c331f75488c7926
nurulhuda789 <hudasallehudin789@gmail.com>
2020-11-22 15:16:23 +0800

    CHAT APPLICATION

---
+++ 4.4.server.c
@@ -0,0 +1,91 @@
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <arpa/inet.h>
+#include <unistd.h>
+#define MAX 80 
+#define SA struct sockaddr 
+   
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int q; 
+    
+    for (;;) 
+    { 
+        bzero(buff, MAX); 
+  
+        read(sockfd, buff, sizeof(buff));  
+        printf("From client:   %s\t To client : ", buff); 
+        bzero(buff, MAX); 
+        q = 0; 
+        while ((buff[q++] = getchar()) != '\n'); 
+        write(sockfd, buff, sizeof(buff)); 
+   
+        if (strncmp("exit", buff, 4) == 0) { 
+            printf("Server Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
+  
+int main() 
+{ 
+    int sockfd, connfd, len; 
+    struct sockaddr_in servaddr, cli; 
+  
+     
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+     	  bzero(&servaddr, sizeof(servaddr)); 
+  
+    
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr =htonl(INADDR_ANY); 
+    servaddr.sin_port = htons( 8888 ); 
+  
+    
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) 
+    { 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully binded..\n"); 
+  
+     
+    if ((listen(sockfd, 5)) != 0) 
+    { 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Server listening..\n"); 
+    
+    	  len = sizeof(cli); 
+  
+     
+    connfd = accept(sockfd, (SA*)&cli, &len); 
+    if (connfd < 0) { 
+        printf("server acccept failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("server acccept the client...\n"); 
+  
+     
+    func(connfd); 
+  
+    close(sockfd); 
+} 
+
+
