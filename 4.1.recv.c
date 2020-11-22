7aa5c99be01551c942927f5c5da18f71f5e698e4
nurulhuda789 <hudasallehudin789@gmail.com>
2020-11-22 10:49:50 +0800

    commit for 2.1.recv.c

--- 4.1.recv.c
+++ 4.1.recv.c
@@ -18,7 +18,7 @@ int main(int argc , char *argv[])
 		
 	server.sin_addr.s_addr = inet_addr("192.168.43.81"); //Please enter the ip address of your Server VM
 	server.sin_family = AF_INET;
-	server.sin_port = htons(22);
+	server.sin_port = htons(8888);
 
 	//Connect to remote server
 	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
